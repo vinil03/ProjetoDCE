ae52c452eea59c88f3d13e58bb6ac3e34249a1d4
Vinicius Meneghel Caseta <vinil_caseta@hotmail.com>
2018-06-05 09:44:01 -0300

    Vinicius

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
