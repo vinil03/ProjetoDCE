f34087bb086165bdcf837f80d2953db6ad6af40c
vinil03 <39656615+vinil03@users.noreply.github.com>
2018-06-05 08:25:02 -0300

    Add files via upload
    camera v0.1

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { HistoricoPage } from '../historico/historico';
 
 /**
  * Generated class for the CameraPage page.
@@ -15,11 +16,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CameraPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  tab1Root = CameraPage;
+  tab2Root = HistoricoPage;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CameraPage');
+  }
+
+  showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'RA',
+      message: "Digite o RA do Aluno",
+      inputs: [
+        {
+          name: 'RA',
+          placeholder: 'RA'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
