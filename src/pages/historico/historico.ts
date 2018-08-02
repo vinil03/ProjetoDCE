ca3768f593074fb112b358f93d24c0669c18f12f
Vinicius Meneghel Caseta <vinil_caseta@hotmail.com>
2018-08-01 23:56:15 -0300

    Cores e lista
    Mudança de cores dinamicamente, de acordo com a respectiva instituição.
    Lista do histórico com apenas um traço entre os elementos

--- src/pages/historico/historico.ts
+++ src/pages/historico/historico.ts
@@ -117,4 +117,41 @@ export class HistoricoPage {
       //busca padrão nos users da msm isntituição
     }
   }
+
+  checarInstituicao(){
+    var DCE = "DCE";
+    var XIX = "XIX";
+    var DIR = "DIR";
+    var ENG = "ENG";
+    var PPM = "PPM";
+    var RI = "RI";
+    var res
+    switch (this.userData.institution) {
+      case "A.A.A. Adhemar F. da Silva": {
+        res = RI;
+        break;
+      }
+      case "A.A.A. PPM": {
+        res = PPM;
+        break;
+      }
+      case "A.A.A. XX de Março": {
+        res = ENG;
+        break;
+      }
+      case "A.A.A. XXVIII de Maio": {
+        res = DIR;
+        break;
+      }
+      case "A.A.A. XIX de Abril": {
+        res = XIX;
+        break;
+      }
+      case "DCE Celso Furtado": {
+        res = DCE;
+        break;
+      }
+    }
+    return res;
+  }
 }
